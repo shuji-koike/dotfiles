aba6357d55a1219939e2d42feaf508dcdc693756
Shuji Koike <shuji.koike@me.com>
2019-12-17 12:11:32 +0900

    Update setup-mvrck.sh

--- scripts/setup-mvrck.sh
+++ scripts/setup-mvrck.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
-# https://brew.sh/#install
-/usr/bin/ruby -e "\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# softwareupdate -ia
 
-brew cask install java8
+# sudo scutil --set HostName mvdt018
+# sudo scutil --set LocalHostName mvdt018
+# sudo scutil --set ComputerName mvdt018
+# dscacheutil -flushcache
+
+xcode-select --install
+
+# https://brew.sh/#install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# brew cask install java8
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
 
 brew cask install docker
 
@@ -12,10 +23,13 @@ brew cask install slack
 brew cask install visual-studio-code
 brew cask install intellij-idea-ce
 
-brew install openssh vim emacs
+brew install openssh git vim emacs
 
-brew install sbt
+brew install sbt scala
 brew install stack
 brew install go
+brew install node
+brew install yarn
+brew install rbenv
 
-mkdir -p ~/mvrck-inc
+mkdir -p ~/github.com/mvrck-inc
