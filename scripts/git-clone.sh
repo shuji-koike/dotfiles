a234caa92e62275a3cdf05ddfb3240827e02a986
Shuji Koike <shuji.koike@me.com>
2020-03-27 22:23:50 +0900

    Create git-clone.sh

---
+++ scripts/git-clone.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash -eu
+[[ $# -gt 0 ]] || exit
+repo="${@: -1}"
+rootdir="${2:-$HOME}"
+owner="${GITHUB_LOGIN:-$(whoami)}"
+host="${GIT_HOST:-github.com}"
+if [[ "$(dirname $repo)" == '.' ]]; then
+  repo="$owner/$repo"
+else
+  owner="$(dirname $repo)"
+fi
+dir="$HOME/$host/$owner"
+url="git@$host:$repo"
+mkdir -p $dir
+cd $dir
+git clone $url
