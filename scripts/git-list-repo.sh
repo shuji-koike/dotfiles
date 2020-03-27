c2335c1a791014eebe497aaaf89acbde2cb7ec35
Shuji Koike <shuji.koike@me.com>
2020-03-27 22:23:32 +0900

    Create git-list-repo.sh

---
+++ scripts/git-list-repo.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash -e
+rootdir="${1:-$HOME/github.com}"
+find $rootdir -maxdepth 3 -type d -name '.git' | while read line; do
+  line="$(dirname $line)"
+  [[ -d $line ]] && echo $line
+done
