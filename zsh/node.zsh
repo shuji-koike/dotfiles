7e081060be990d3b087154df82cceca4a61b135d
Shuji Koike <shuji.koike@me.com>
2020-04-13 14:13:28 +0900

    Update node.zsh

--- zsh/node.zsh
+++ zsh/node.zsh
@@ -1,4 +1,4 @@
-alias ss='npm start'
-alias nn='npm run'
+alias ns='npm start'
+alias nr='npm run'
 alias dcnpm='docker-compose run --rm ${APP:-app} npm'
 alias dcnpx='docker-compose run --rm ${APP:-app} npx'
